5ec684c136a62e4fe5a2149f58783d6c70cff3bf
贺兰星辰 <shaokeyibb@126.com>
2020-10-31 23:46:52 +0800

    向主类添加实用函数

--- src/main/java/kim/minecraft/citycore/CityCore.kt
+++ src/main/java/kim/minecraft/citycore/CityCore.kt
@@ -1,12 +1,31 @@
 package kim.minecraft.citycore
 
 import io.izzel.taboolib.loader.Plugin
+import io.izzel.taboolib.loader.PluginBase
+import io.izzel.taboolib.module.config.TConfig
 
 @Suppress("unused")
 object CityCore : Plugin() {
 
-    override fun onEnable() {
+    private val config: TConfig by lazy {
+        TConfig.create(plugin, "config.yml")
+    }
 
+    fun Any.getPlugin(): PluginBase = plugin
+
+    fun Any.getConfig(): TConfig = config
+
+    fun Any.getLocale(key: String): String = config.getStringColored("Message.$key", "lost locale for Message.$key")
+
+    fun Any.getLocaleList(key: String): List<String> = config.getStringListColored("Message.$key")
+
+    fun Any.info(msg: String) = plugin.logger.info(msg)
+
+    fun Any.getAdminPermission(): String = config.getString("Global.AdminPermission", "citycore.admin")!!
+
+    override fun onEnable() {
+        if (plugin.getResource("config.yml") == null)
+            plugin.saveResource("config.yml", false)
     }
 
     override fun onDisable() {
